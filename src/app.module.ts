97b021e461f34446dedf340478e5379c51c77fab
joaomoura <iaiopvid@yahoo.com.br>
2022-07-29 15:54:38 -0300

    Dry app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-// import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from '@hapi/joi';
 import { DatabaseModule } from './database/database.module';
 
@@ -11,14 +10,8 @@ import { UsersModule } from './users/users.module';
 import { CompaniesModule } from './companies/companies.module';
 import { AutomobilesModule } from './automobiles/automobiles.module';
 
-import User from './users/user.entity';
-import { Automobile } from './automobiles/automobile.entity';
-import { Company } from './companies/company.entity';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { ParksModule } from './parks/parks.module';
-import { Park } from './parks/park.entity';
-
-const entities = [User, Automobile, Company, Park];
 
 @Module({
   imports: [
@@ -35,16 +28,6 @@ const entities = [User, Automobile, Company, Park];
         PORT: Joi.number(),
       })
     }),
-    // TypeOrmModule.forRoot({
-    //   type: process.env.DATABASE_TYPE as any,
-    //   host: process.env.DATABASE_HOST,
-    //   port: parseInt(process.env.DATABASE_PORT),
-    //   username: process.env.DATABASE_USER,
-    //   password: process.env.MYSQL_ROOT_PASSWORD,
-    //   database: process.env.MYSQL_DATABASE,
-    //   entities: entities,
-    //   synchronize: true,
-    // }),
     DatabaseModule,
     UsersModule,
     AuthenticationModule,
